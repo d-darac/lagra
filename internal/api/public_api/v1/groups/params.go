2b2cf711f9235167de41e13ee6b634ce9d253483
d-darac <dragicevic.dario@gmail.com>
2026-05-01 08:16:14 +0100

    optimise struct field alignment

--- internal/api/public_api/v1/groups/params.go
+++ internal/api/public_api/v1/groups/params.go
@@ -12,16 +12,16 @@ type GetGroupParams struct {
 }
 
 type ListGroupsParams struct {
-	EndingBefore  *string `json:"ending_before"`
-	StartingAfter *string `json:"starting_after"`
-	Limit         *int    `json:"limit"`
 	// *com.PaginationParams
 	// CreatedAt   *com.TimeRange `json:"created_at" validate:"omitnil"`
-	ParentGroup *string `json:"parent_group" validate:"omitnil,id"`
-	Description *string `json:"description" validate:"omitnil"`
-	Name        *string `json:"name" validate:"omitnil"`
 	// UpdatedAt   *com.TimeRange `json:"updated_at" validate:"omitnil"`
-	Expand []string `json:"expand" validate:"omitnil,dive,oneof=parent_group"`
+	EndingBefore  *string  `json:"ending_before"`
+	StartingAfter *string  `json:"starting_after"`
+	Description   *string  `json:"description" validate:"omitnil"`
+	Name          *string  `json:"name" validate:"omitnil"`
+	ParentGroup   *string  `json:"parent_group" validate:"omitnil,id"`
+	Limit         *int     `json:"limit"`
+	Expand        []string `json:"expand" validate:"omitnil,dive,oneof=parent_group"`
 }
 
 type UpdateGroupParams struct {
