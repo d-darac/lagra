edec45932214273107263414eda0c67633ba6b61
Dario Dragicevic <dragicevic.dario@gmail.com>
2026-05-09 17:50:07 +0100

    add set package

---
+++ pkg/set/set.go
@@ -0,0 +1,20 @@
+package set
+
+type Set[T comparable] map[T]struct{}
+
+func (s Set[T]) Add(v T) {
+	s[v] = struct{}{}
+}
+
+func (s Set[T]) Has(v T) bool {
+	_, ok := s[v]
+	return ok
+}
+
+func (s Set[T]) Remove(v T) {
+	delete(s, v)
+}
+
+func (s Set[T]) Clear() {
+	clear(s)
+}
